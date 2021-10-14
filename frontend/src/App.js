230ae2058dd4c7129c0a72fe9b6cbff1ad00070f
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2021-10-14 10:07:27 +0530

    Added Footer Component- It will stick to footer , once more content is added !

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from './Components/Footer Files/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      {/* Note : Once More Content will be added to website, It will eventually stick Down */}
+      <Footer/> 
     </div>
   );
 }

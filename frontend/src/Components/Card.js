49a9529eecb3eab37529ba4c6876d8b0b6f5e41b
Mohammad Bilal <mdbilal7620@gmail.com>
2021-10-15 20:05:18 +0530

    Member section

---
+++ frontend/src/Components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import '../styles/card.css';
+
+const Card = ({data}) => {
+    return ( 
+        <div className="single-card">
+            <Flippy className="flip-main"
+                flipOnHover={true} 
+                flipOnClick={true} 
+                flipDirection="horizontal"
+            >   
+                {/* prim rgb(187, 93, 26) */}
+                {/* second  rgb(247, 215, 192) */}
+            <FrontSide className="front">
+                    <img alt="members-pic" className="mem-pic" src={data.img}/>
+                    <h4 id="mem-name">{data.name}</h4>
+                    <h6>{data.prof}</h6>
+            </FrontSide>
+            <BackSide className="back">
+                    <p className="mem-desc">{data.desc}</p>
+                    <div className="mem-socials">
+                    <a href={data.socials.portfolio}><i class="fas fa-id-card"></i></a>
+                    <a href={data.socials.email}><i class="fas fa-envelope"></i></a>
+                    <a href={data.socials.linkedIn}><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    <a href={data.socials.github}><i class="fa fa-github" aria-hidden="true"></i></a>
+                    <a href={data.socials.twitter}><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </div>
+            </BackSide>
+            </Flippy>
+        </div>
+     );
+}
+ 
+export default Card;

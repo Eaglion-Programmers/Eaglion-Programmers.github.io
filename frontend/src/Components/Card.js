98cc67a2b26ee2d67233db8fe3f8df953c5df5db
Mohammad Bilal <mdbilal7620@gmail.com>
2021-10-15 20:35:52 +0530

    open link in new tab

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -20,11 +20,11 @@ const Card = ({data}) => {
             <BackSide className="back">
                     <p className="mem-desc">{data.desc}</p>
                     <div className="mem-socials">
-                    <a href={data.socials.portfolio}><i class="fas fa-id-card"></i></a>
-                    <a href={data.socials.email}><i class="fas fa-envelope"></i></a>
-                    <a href={data.socials.linkedIn}><i class="fa fa-linkedin" ></i></a>
-                    <a href={data.socials.github}><i class="fa fa-github" ></i></a>
-                    <a href={data.socials.twitter}><i class="fa fa-twitter" ></i></a>
+                    <a href={data.socials.portfolio} target="_blank"><i class="fas fa-id-card"></i></a>
+                    <a href={data.socials.email} target="_blank"><i class="fas fa-envelope"></i></a>
+                    <a href={data.socials.linkedIn} target="_blank"><i class="fa fa-linkedin" ></i></a>
+                    <a href={data.socials.github} target="_blank"><i class="fa fa-github" ></i></a>
+                    <a href={data.socials.twitter} target="_blank"><i class="fa fa-twitter" ></i></a>
                     </div>
             </BackSide>
             </Flippy>

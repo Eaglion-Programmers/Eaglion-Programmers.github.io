2c44468ef90850eef7abd5b97e347c73aea92330
Mohammad Bilal <mdbilal7620@gmail.com>
2021-10-16 08:19:27 +0530

    jsx depploy  issue resolved

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -20,11 +20,21 @@ const Card = ({data}) => {
             <BackSide className="back">
                     <p className="mem-desc">{data.desc}</p>
                     <div className="mem-socials">
-                    <a href={data.socials.portfolio} target="_blank"><i class="fas fa-id-card"></i></a>
-                    <a href={data.socials.email} target="_blank"><i class="fas fa-envelope"></i></a>
-                    <a href={data.socials.linkedIn} target="_blank"><i class="fa fa-linkedin" ></i></a>
-                    <a href={data.socials.github} target="_blank"><i class="fa fa-github" ></i></a>
-                    <a href={data.socials.twitter} target="_blank"><i class="fa fa-twitter" ></i></a>
+                    <a href={data.socials.portfolio} target="_blank" rel="noopener noreferrer">
+                        <i class="fas fa-id-card"></i>
+                    </a>
+                    <a href={data.socials.email} target="_blank" rel="noopener noreferrer">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                    <a href={data.socials.linkedIn} target="_blank" rel="noopener noreferrer">
+                        <i class="fa fa-linkedin" ></i>
+                    </a>
+                    <a href={data.socials.github} target="_blank" rel="noopener noreferrer">
+                        <i class="fa fa-github" ></i>
+                    </a>
+                    <a href={data.socials.twitter} target="_blank" rel="noopener noreferrer">
+                        <i class="fa fa-twitter" ></i>
+                    </a>
                     </div>
             </BackSide>
             </Flippy>

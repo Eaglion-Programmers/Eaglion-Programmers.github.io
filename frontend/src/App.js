9b26397e37ca867d12ce6121b768d5e308391dd6
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2021-10-18 22:21:11 +0530

    Initiated About Us Page - will be updated Soon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,11 @@ function App() {
     <div className="App">
       <Navbar/>
       {/* Disabled for now making its overlapping body enable it at the end */}
-      <h5 className="sec-heading">ABOUT</h5>
+      {/* <h5 className="sec-heading">ABOUT</h5> */}
       <AboutUs/>
-      <h5 className="sec-heading">MEMBERS</h5>
+      {/* <h5 className="sec-heading">MEMBERS</h5> */}
       <Members/>
-      <h5 className="sec-heading">CONTACT</h5>
+      {/* <h5 className="sec-heading">CONTACT</h5> */}
       <Contact/>
       <Footer/> 
     </div>

4048431c8079b7e713edd9081c54d8432fc77800
gayatri2 <gayatri.godbole02@gmail.com>
2021-10-20 08:55:04 +0530

    Added routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar';
-import AboutUs from './Components/AboutUs';
-import Members from './Components/Members';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import AboutUs from "./Components/AboutUs";
+import Members from "./Components/Members";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AboutUs/>
-      <Members/>
-      <Contact/>
-      <Footer/> 
+      <Router>
+        <Switch>
+        <Route exact path="/">
+            <Navbar />
+            <Footer />
+          </Route>
+          <Route exact path="/about">
+            <Navbar />
+            <AboutUs />
+            <Footer />
+          </Route>
+          <Route exact path="/members">
+            <Navbar />
+            <Members />
+
+            <Footer />
+          </Route>
+          <Route exact path="/contact">
+            <Navbar />
+            <Contact />
+            <Footer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

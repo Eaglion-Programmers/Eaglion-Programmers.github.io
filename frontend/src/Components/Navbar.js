fe262b982b2946a07c775230e13fd2e01a21c6b9
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2021-10-20 14:27:52 +0530

    fixed minor bugs

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,48 +1,29 @@
 import React from 'react';
 import '../styles/navbar.css';
-import Members from './Members';
-import AboutUs from './AboutUs';
-import Footer from './Footer';
+import { Link } from 'react-router-dom';
+
+
 const Navbar = () => {
-    return ( 
+    return (
         <>
-        <nav>
-            <div className="logo">
-                <strong>EAGLION</strong>
-            </div>
-            <input type="checkbox" id="click"/>
-            <label htmlFor="click" className="menu-btn">
+            <nav>
+                <div className="logo">
+                    <strong>EAGLION</strong>
+                </div>
+                <input type="checkbox" id="click" />
+                <label htmlFor="click" className="menu-btn">
                     <i className="fas fa-bars"></i>
-            </label>
-            <ul>
-                <li ><a className="active" href="/">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#members">Members</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="https://auto-joiner-eaglion.herokuapp.com/">Join Community</a></li>
-            </ul>
-        </nav>
-        
-        
-        <section id="about">
-        <h5 className="sec-heading">ABOUT</h5>
-            <AboutUs/>
-        </section>
-
-       
-        <section id="members">
-        <h5 className="sec-heading">MEMBERS</h5>
-            <Members/>
-        </section>
-
-        <section id="contact">
-        <h5 className="sec-heading">CONTACT</h5>
-            <Footer/>
-        </section>
-        
-
-    </>
-     );
+                </label>
+                <ul>
+                    <li ><Link className="active" to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/members">Members</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><a href="https://auto-joiner-eaglion.herokuapp.com/">Join Community</a></li>
+                </ul>
+            </nav>
+        </>
+    );
 }
- 
+
 export default Navbar;
